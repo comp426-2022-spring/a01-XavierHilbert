6877d6367188cd499563d05e35eb3b311462ce19
XavierHilbert <xavierh@email.unc.edu>
2022-01-27 15:27:31 -0500

    fixed displaying data

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = require('minimist')(process.argv.slice(2))
 // Define allowed argument name 'port'.
-const port = args["port"] / process.env.PORT / 3000
+const port = args["port"] || process.env.PORT || 3000
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 
@@ -37,10 +37,8 @@ fs.readFile('./www/index.html', 'utf8' , (err, data) => {
 const server =  http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
-    res.end('./www/index.html')
+    res.end(data)
   })
-
-
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
